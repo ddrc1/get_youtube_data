bb376bd642c1cd5911f4af1d6e933a1dec4148ce
danielrotheia <danielrotheia@gmail.com>
2022-06-03 12:03:06 -0300

    Update script_jsonl.py

--- script_jsonl.py
+++ script_jsonl.py
@@ -1,22 +1,26 @@
 import os, json
 
-path = '.\\canais_json'
-directory = '.\\'
-separador = "\\"
+def save_jsonl(outfile, path):
+    file_json = open(path, 'r')
+    datastore = json.load(file_json)
+    json.dump(datastore, outfile)
+    outfile.write("\n")
+
+
+path = './channel_data'
+directory = './jsonl_data/'
+
+if not os.path.exists(directory):
+    os.mkdir(directory)
+    
 # r=root, d=directories, f = files
+print("Montando arquivos...")
 for r, d, f in os.walk(path):
     for arq in f:
         if ".json" in arq and len(d) > 0:
             with open(directory + 'video.jsonl', 'a+') as outfile:
-                file_json = open(str(r) + separador + str(arq), 'r')
-                datastore = json.load(file_json)
-                json.dump(datastore, outfile)
-                outfile.write("\n")
-            print("gravando video")
+                save_jsonl(outfile, str(r) + "/" + str(arq))
         elif ".json" in arq and not d:
             with open(directory + 'comment.jsonl', 'a+') as outfile:
-                file_json = open(str(r) + separador + str(arq), 'r')
-                datastore = json.load(file_json)
-                json.dump(datastore, outfile)
-                outfile.write("\n")
-            print("gravando comentario")
+                save_jsonl(outfile, str(r) + "/" + str(arq))
+print("Terminado")
